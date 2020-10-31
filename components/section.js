7eae8d526e6256b3b177444d4e6a1a2f7e84a761
magiobus <aalbertobus@gmail.com>
2020-10-30 23:24:44 -0700

    Down Triangle in section fixed

--- components/section.js
+++ components/section.js
@@ -26,20 +26,22 @@ const Section = ({
         {/* option to triangle 100%width colors */}
         {triangleBiBottom && (
             <div className="bicolortriangleContainer" data-negative="false">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none" width="100%" height="24px">
                     <path  d="M0,6V0h1000v100L0,6z"></path>
                 </svg>
             </div>
         )}
         
         {/* //Option to have triangle down section */}
-            {miniTriangleDown && (
-                <Flex fill={miniTriangleColor} display={["none", "none", "flex", "flex"]}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 10" preserveAspectRatio="none">
-                        <path className="elementor-shape-fill" d="M350,10L340,0h20L350,10z" ></path>
-                    </svg>		
-                </Flex>
-            )}
+        {miniTriangleDown && (
+            <Flex fill={miniTriangleColor} display={["none", "none", "flex", "flex"]} >
+            <div data-negative="false" className="miniTriangleDownContainer">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 10" preserveAspectRatio="none" width="100%" height="23px">
+                    <path  d="M350,10L340,0h20L350,10z" ></path>
+                </svg>	
+            </div>	
+            </Flex>
+        )}
 
         <style jsx>{`
             .bicolortriangleContainer{
@@ -48,11 +50,9 @@ const Section = ({
                 transform: translateX(0) rotateY(180deg);
                 height: 24px;
                 width: 100%;
-                top: -1;
             }
 
-            .bicolortriangleContenter > svg{
-                height: 24px;
+            .miniTriangleDownContainer{
                 width: 100%;
             }
         `}</style>
