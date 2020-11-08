7355a946ae32559f2b997335b6a56b6ae3b05d65
magiobus <aalbertobus@gmail.com>
2020-11-07 18:30:19 -0700

    faqs section

--- components/faqItem.js
+++ components/faqItem.js
@@ -1,10 +1,23 @@
-import { Flex, Text } from "@chakra-ui/core";
+import { Flex, Text, Collapse, Button} from "@chakra-ui/core";
+import {useState} from 'react';
 
 const FaqItem = ({question, answer}) => {
+    const [show, setShow] = useState(false);
+
+    const handleToggle = () => setShow(!show);
+
+
     return (
-        <Flex my={5} direction="column">
-            <Text><b>{question}</b></Text>
-            <Text>{answer}</Text>
+        <Flex direction="column" width="100%" >
+            <Flex my={5} px={5} direction="column" alignItems="center" justify="center">
+                <Text textAlign="center" fontSize={["1em", "1.5em", "2em","2em"]} fontFamily="cpc.gothamMedium" lineHeight={["1em"]}><b>{question}</b></Text>
+                <Button bg="cpc.red" color="cpc.white" _hover={{bg: "cpc.red"}} size="sm" onClick={handleToggle} mt="1rem">
+                     {show ? "Cerrar" : "Ver Respuesta"}
+                </Button>
+            </Flex>
+            <Flex>
+                <Collapse  px={5} isOpen={show} fontSize={["1em", "1.25em", "1.25em","1.5em"]} fontFamily="cpc.gothamMedium" mt={3}>{answer}</Collapse>
+            </Flex>
         </Flex>
 
     )
--- pages/faq.js
+++ pages/faq.js
@@ -4,14 +4,33 @@ import Content from '../components/content';
 import FaqItem from '../components/faqItem';
 import Section from '../components/section';
 import {getAllFaqs} from '../lib/api'
+import customtheme from '../customtheme.js'
+import { Flex, Text } from '@chakra-ui/core';
+
+
+
 
 export default function FaqPage({faqs}) {
+    const {colors} = customtheme
+
     return (
         <Layout title="Preguntas frecuentes de - CPC Anticorrupcion">
             <Header position="fixed"/>
             <Content>
-                <Section bg="cpc.yellow" color="cpc.white">
-                    {faqs.map(faq => <FaqItem key={faq._id} question={faq.question} answer={faq.answer}/>)}
+                {/* Top SECTION */}
+                <Section bg="cpc.red" color="cpc.white" 
+                    splitBiBottom="true"
+                    splitBiBottomColorOne={colors.cpc.red}
+                    splitBiBottomColorTwo={colors.cpc.white}
+                >
+                    <Text fontSize={["1em", "1.5em", "3em", "3em"]} fontFamily="cpc.gothamBold" textAlign="center" lineHeight="1.18em">
+                        <b>Preguntas <br/> frecuentes del cpc</b>
+                    </Text>
+                </Section>  
+                <Section bg="cpc.white" color="cpc.red">
+                    <Flex direction="column" height="100vh">
+                        {faqs.map(faq => <FaqItem key={faq._id} question={faq.question} answer={faq.answer}/>)}
+                    </Flex>
                 </Section>
             </Content>
         </Layout>
