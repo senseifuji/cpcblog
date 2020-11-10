e9518a076ccbd667c0705611fc3b6bb83ead2ae1
magiobus <aalbertobus@gmail.com>
2020-11-10 00:01:17 -0700

    Google Analytics fixed

--- components/layout.js
+++ components/layout.js
@@ -8,6 +8,15 @@ const Layout = ({children, direction="row"}) => {
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="css/fontface.css"/>
                 <script src="https://kit.fontawesome.com/03f0c5f1a0.js" crossorigin="anonymous"></script>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-LE69ZN8M4Q"></script>
+                <script async dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', G-LE69ZN8M4Q);`
+                    }}
+                />
+
             </Head>
             <Flex direction={direction}>
                 {children}
