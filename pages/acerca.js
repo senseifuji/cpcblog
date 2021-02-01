6f195a62970eadc79343a8ca3efaa41a80db2bba
cpcwebchih <73916365+cpcwebchih@users.noreply.github.com>
2021-02-01 06:38:03 +0000

    Update acerca.js

--- pages/acerca.js
+++ pages/acerca.js
@@ -104,7 +104,7 @@ export default function AboutPage() {
                    <Flex justify="space-evenly" direction={["column", 'column', 'row', 'row']}>
                         <Flex direction="column" alignItems="center" width={["100%", "100%", "70%", "60%" ]}>
                             <Image src="/images/sistemaestatal.svg" alt="SEA" width={["15em", "15em", "20em", "20em"]} />                            
-                            <Text my={4} lineHeight="1.1em" fontFamily="cpc.spaceMonoRegular" fontSize={["1em", "1.2em", "1.5em", "1.5em"]} fontWeight="300" textAlign={["left", "center", "center", "left"]} my={["1rem", "0", "0", "0"]} pl={["1em", "0", "0", "0"]} pr={[".5em", "0", "0", "0"]}>
+                            <Text lineHeight="1.1em" fontFamily="cpc.spaceMonoRegular" fontSize={["1em", "1.2em", "1.5em", "1.5em"]} fontWeight="300" textAlign={["left", "center", "center", "left"]} my={["1rem", "0", "0", "0"]} pl={["1em", "0", "0", "0"]} pr={[".5em", "0", "0", "0"]}>
                                 Ente creado para coordinar acciones
                                 interinstitucionales orientadas a:
                                 combatir, prevenir, disuadir, sancionar
