5bbe83b16b69ceacf1f1ddff45dedaab5a0395c3
cpcwebchih <73916365+cpcwebchih@users.noreply.github.com>
2021-02-05 20:49:16 +0000

    Update acerca.js

--- pages/acerca.js
+++ pages/acerca.js
@@ -329,13 +329,16 @@ export default function AboutPage() {
                             en un espacio de trabajo denominado  
                         </Text>
                         <Image src="/images/redanticorrupcion.svg" alt="Red Anticorrupción" 
-                        height={["6em", "6em", "7em", "7em"]} my={5}/>
+                        height={["6em", "6em", "7em", "7em"]} 
+                        mt={["0", 5, 5, 5]}
+                        mb={5}/>
                     </Flex>
 
                     <Flex direction={["column", "row", "row", "row"]} 
                     width={["90%","500px", "600px", "600px"]} justify="center" alignItems="center">
                         <Image src="/images/4CPCs.svg" alt="4 integrantes del CPC" 
-                        height={["6em", "6em", "7em", "7em"]} mt="30px"/>
+                        height={["6em", "6em", "7em", "7em"]} mt={["30px",5, 5, 5]}
+                        mb={["0px", 5, 5, 5]}/>
                         <Text color="cpc.black" width={["100%", "400px", "400px", "400px"]} 
                         px={[".7em", "1em", "1em", "1em"]} 
                         py={["0.5em", "1em", "1em", "1em"]} fontFamily="cpc.gothamCondensedBook" 
@@ -370,7 +373,10 @@ export default function AboutPage() {
                     <Flex direction={["column", "row", "row", "row"]} 
                     width={["90%","500px", "600px", "600px"]} justify="center" alignItems="center">
                         <Image src="/images/ana2.svg" alt="Presidenta del CPC" 
-                        height={["6em", "6em", "7em", "7em"]} mt="30px" mx={5}/>
+                        height={["6em", "6em", "7em", "7em"]} 
+                        mt={["0", 5, 5, 5]}
+                        mb={["0", 5, 5, 5]}
+                        mx={5}/>
                         <Text color="cpc.black" width={["100%", "400px", "400px", "400px"]} 
                         px={[".7em", "1em", "1em", "1em"]} 
                         py={["0.5em", "1em", "1em", "1em"]} fontFamily="cpc.gothamCondensedBook" 
