1222ccb92603fd7e555b976cd88790c3568693a2
cpcwebchih <73916365+cpcwebchih@users.noreply.github.com>
2021-02-06 02:58:40 +0000

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -6,6 +6,7 @@ import Section from '../components/section';
 import {getAllFaqs} from '../lib/api'
 import customtheme from '../customtheme.js'
 import { Flex, Text } from '@chakra-ui/core';
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import CpcSeo from '../components/cpcseo'
 
@@ -30,7 +31,7 @@ export default function FaqPage({faqs}) {
                         <Text fontSize={["1.75em", "1.75em", "2em", "2em"]} fontFamily="cpc.gothamBold" 
                         fontWeight="300" textAlign="center" lineHeight={["1em", "1em", "1.18em", "1.18em"]} 
                         letterSpacing={["-.05em", "0", "0", "0"]} width={["100%", "80%", "80%", "75%"]}>
-                            Preguntas frecuentes hechas al CPC
+                            Preguntas frecuentes hechas al Comité de Participación Ciudadana
                         </Text>
                     </Section> 
                     <Section bg="cpc.white" color="cpc.black">
@@ -42,8 +43,18 @@ export default function FaqPage({faqs}) {
                             fontSize={["1.3em", "1.3em", "1.4em", "1.4em"]} 
                             textAlign="left" lineHeight={["1.2em", "1.2em", "1.2em", "1.2em"]}>
                                 Aquí vamos a estar resolviendo las preguntas más frecuentes que
-                                nos hagan llegar al CPC tanto por Solicitudes de Transparencia como 
-                                por la sección de Contacto.
+                                nos hagan llegar al Comité de Participación Ciudadana de Chihuahua 
+                                (CPC) tanto por 
+                                <Link href="https://buscador.plataformadetransparencia.org.mx/web/guest/buscadornacional?buscador=%22comit%C3%A9%20de%20participaci%C3%B3n%20ciudadana%22&coleccion=5">
+                                <a className="redlink"> 
+                                    {" "}Solicitudes de Transparencia 
+                                </a>
+                            </Link> como por la sección de 
+                            <Link href="/contacto">
+                                <a className="redlink">
+                                {" "}Contacto.
+                                </a>
+                            </Link>
                         </Text>
                     </Flex>
                     </Section> 
@@ -53,6 +64,17 @@ export default function FaqPage({faqs}) {
                         </Flex>
                     </Section>
                 </Content>
+                <style jsx>{`
+                    .redlink {
+                        color: ${colors.cpc.red};
+                        cursor: pointer;
+                        text-decoration: underline;
+                    }
+                    .redText {
+                        color: ${colors.cpc.red};
+                    }
+                `}
+                </style>
             </Layout>
         </>
     )
