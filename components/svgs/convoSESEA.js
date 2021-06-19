312536588399daacbefadacb841b327290d2d70e
cpcwebchih <73916365+cpcwebchih@users.noreply.github.com>
2021-06-19 03:02:45 +0000

    Update convoSESEA.js

--- components/svgs/convoSESEA.js
+++ components/svgs/convoSESEA.js
@@ -1,90 +1,98 @@
-
-import { Icon, createIcon } from "@chakra-ui/core"
-
-const Logo = ({logofill, letrasfill, width, height}) =>  {
-
+const ConvoSESEA = ({size}) =>  {
   return (
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 418.85 151.26" style="enable-background:new 0 0 418.85 151.26;" xml:space="preserve">
-<style type="text/css">
-	.st0{fill:#00BDFF;}
-	.st1{fill:#FFFFFF;}
-	.st2{opacity:0.74;clip-path:url(#SVGID_2_);}
-	.st3{fill:none;stroke:#3FA9F5;stroke-width:1.1803;stroke-miterlimit:10;}
-	.st4{fill:none;stroke:#3FA9F5;stroke-width:4.6846;stroke-miterlimit:10;}
-	.st5{fill:none;stroke:#3FA9F5;stroke-width:8.189;stroke-miterlimit:10;}
-	.st6{fill:none;stroke:#3FA9F5;stroke-width:11.6933;stroke-miterlimit:10;}
-	.st7{fill:none;stroke:#3FA9F5;stroke-width:15.1976;stroke-miterlimit:10;}
-	.st8{fill:none;stroke:#3FA9F5;stroke-width:18.702;stroke-miterlimit:10;}
-	.st9{fill:none;stroke:#3FA9F5;stroke-width:22.2063;stroke-miterlimit:10;}
-	.st10{fill:none;stroke:#3FA9F5;stroke-width:25.7107;stroke-miterlimit:10;}
-	.st11{fill:none;stroke:#3FA9F5;stroke-width:29.215;stroke-miterlimit:10;}
-	.st12{fill:none;stroke:#3FA9F5;stroke-width:32.7193;stroke-miterlimit:10;}
-	.st13{fill:none;stroke:#3FA9F5;stroke-width:36.2237;stroke-miterlimit:10;}
-	.st14{fill:none;stroke:#3FA9F5;stroke-width:39.728;stroke-miterlimit:10;}
-	.st15{fill:none;stroke:#3FA9F5;stroke-width:43.2324;stroke-miterlimit:10;}
-	.st16{opacity:0.74;clip-path:url(#SVGID_4_);}
-	.st17{opacity:0.74;clip-path:url(#SVGID_6_);}
-	.st18{opacity:0.74;clip-path:url(#SVGID_8_);}
-	.st19{font-family:'SpaceMono-Italic';}
-	.st20{font-size:29.8481px;}
-	.st21{font-family:'SpaceMono-BoldItalic';}
-	.st22{font-size:31.0817px;}
-	.st23{fill:#1A1B2F;}
-	.st24{opacity:0.74;clip-path:url(#SVGID_10_);}
-</style>
+   viewBox="0 0 418.85 151.26" style="enable-background:new 0 0 418.85 151.26;" xml:space="preserve">
+
 <g>
-	<g>
-		<polygon class="st0" points="248.71,98.1 43.24,98.1 50.48,38.19 255.1,38.19 		"/>
-		<g>
-			<path class="st0" d="M263.97,38.76h34l6.38,59.85h-21.46V79.38h-4.16v19.23h-21.14L263.97,38.76z M275.55,63.85l1.29,3.03
-				l9.55-4.05l-1.29-3.03L275.55,63.85z"/>
-			<path class="st0" d="M305.14,38.76h19.31l7.67,24.1l1.3-0.29l-1.04-4.87c-0.8-3.91-1.85-8.41-1.85-12.48v-6.47h21.39v59.85H332.6
-				l-7.67-24.1l-1.29,0.27l1.04,4.87c0.8,3.91,1.85,8.43,1.85,12.5v6.46h-21.39V38.76z"/>
-			<polygon class="st0" points="395.23,57.75 385.34,57.75 385.34,98.61 363.95,98.61 363.95,57.75 354.14,57.75 354.14,38.76 
-				395.23,38.76 			"/>
-			<rect x="397.46" y="38.76" class="st0" width="21.39" height="59.85"/>
-			<path class="st1" d="M318.86,150.49c-0.44-2.22-0.76-5.21-0.76-7.88v-11.94c0-1.14-0.44-2.03-1.33-2.03h-1.4v21.85h-5.97V99.68
-				h7.69c2.98,0,6.73,1.53,6.73,8.01v12.96c0,2.42-1.14,3.94-2.73,5.27c1.59,1.33,2.86,2.86,2.86,5.27v10.8
-				c0,3.24,0.44,6.03,0.83,8.51H318.86z M317.97,107.81c0-1.46-0.38-2.03-1.08-2.03h-1.52v17.09h1.52c0.7,0,1.08-0.57,1.08-2.03
-				V107.81z"/>
-			<path class="st1" d="M333.6,151.13c-4.13,0-7.18-3.56-7.18-8.7V99.68h5.66v43.19c0,1.14,0.57,1.84,1.52,1.84
-				c1.02,0,1.59-0.7,1.59-1.84V99.68h5.4v42.75C340.59,147.57,337.73,151.13,333.6,151.13"/>
-			<path class="st1" d="M264.94,151.13c-4.13,0-7.3-3.56-7.3-8.7v-34.68c0-5.15,3.18-8.7,7.3-8.7c4.13,0,7.31,3.56,7.31,8.7v12.08
-				l-5.78-1.66V107.3c0-1.14-0.57-1.84-1.53-1.84c-0.95,0-1.52,0.7-1.52,1.84v35.57c0,1.14,0.57,1.84,1.52,1.84
-				c0.95,0,1.53-0.7,1.53-1.84v-10.86h5.78v10.42C272.25,147.57,269.07,151.13,264.94,151.13"/>
-			<path class="st1" d="M282.16,151.13c-4.13,0-7.56-3.56-7.56-8.7v-34.68c0-5.15,3.43-8.7,7.56-8.7c4.13,0,7.56,3.56,7.56,8.7
-				v34.68C289.71,147.57,286.28,151.13,282.16,151.13 M283.68,107.3c0-1.14-0.57-1.84-1.52-1.84c-1.02,0-1.59,0.7-1.59,1.84v35.57
-				c0,1.14,0.57,1.84,1.59,1.84c0.95,0,1.52-0.7,1.52-1.84V107.3z"/>
-			<path class="st1" d="M301.72,150.49c-0.44-2.22-0.76-5.21-0.76-7.88v-11.94c0-1.14-0.44-2.03-1.33-2.03h-1.4v21.85h-5.97V99.68
-				h7.69c2.98,0,6.73,1.53,6.73,8.01v12.96c0,2.42-1.14,3.94-2.73,5.27c1.59,1.33,2.86,2.86,2.86,5.27v10.8
-				c0,3.24,0.44,6.03,0.83,8.51H301.72z M300.83,107.81c0-1.46-0.38-2.03-1.08-2.03h-1.52v17.09h1.52c0.7,0,1.08-0.57,1.08-2.03
-				V107.81z"/>
-			<path class="st1" d="M350.87,129.28h-1.71v21.22h-5.97V99.68h7.69c2.98,0,6.73,1.53,6.73,8.01v13.66
-				C357.61,127.75,353.86,129.28,350.87,129.28 M351.76,107.81c0-1.46-0.38-2.03-1.08-2.03h-1.52v17.4h1.52
-				c0.7,0,1.08-0.57,1.08-2.03V107.81z"/>
-			<path class="st1" d="M366.43,151.26c-4.13,0-7.3-3.56-7.3-8.7v-34.68c0-5.15,3.18-8.7,7.3-8.7c4.13,0,7.31,3.56,7.31,8.7v12.08
-				l-5.78-1.66v-10.86c0-1.14-0.57-1.84-1.53-1.84c-0.95,0-1.52,0.7-1.52,1.84V143c0,1.14,0.57,1.84,1.52,1.84
-				c0.95,0,1.53-0.7,1.53-1.84v-10.86h5.78v10.42C373.74,147.7,370.56,151.26,366.43,151.26"/>
-			<rect x="376.47" y="99.8" class="st1" width="5.97" height="50.82"/>
-			<path class="st1" d="M392.73,99.17c-4.13,0-7.56,3.56-7.56,8.7v34.68c0,5.14,3.43,8.7,7.56,8.7c4.13,0,7.56-3.56,7.56-8.7v-34.68
-				C400.29,102.73,396.86,99.17,392.73,99.17 M394.25,143c0,1.14-0.57,1.84-1.52,1.84c-1.02,0-1.59-0.7-1.59-1.84v-35.57
-				c0-1.14,0.57-1.84,1.59-1.84c0.95,0,1.52,0.7,1.52,1.84V143z"/>
-			<polygon class="st1" points="397.17,94.44 388.51,98.61 388.51,96.29 397.17,92.12 			"/>
-			<polygon class="st1" points="413.88,150.62 407.78,122.86 407.78,150.62 402.82,150.62 402.82,99.8 408.86,99.8 413.94,128.01 
-				413.94,99.8 418.83,99.8 418.83,150.62 			"/>
-			<polygon class="st0" points="418.83,150.62 413.88,150.62 257.64,105.91 257.64,99.04 418.82,145.16 			"/>
-		</g>
-		<text transform="matrix(1 0 0 1 56.7549 65.0283)"><tspan x="0" y="0" class="st1 st19 st20">Secretaría</tspan><tspan x="19.01" y="24.83" class="st1 st19 st20">Ejecutiva</tspan></text>
-	</g>
-	<g>
-		<text transform="matrix(1 0 0 1 0 26.4189)" class="st1 st21 st22">Buscamos titular de la</text>
-	</g>
+  <g>
+    <polygon className="st0" points="248.71,98.1 43.24,98.1 50.48,38.19 255.1,38.19     "/>
+    <g>
+      <path className="st0" d="M263.97,38.76h34l6.38,59.85h-21.46V79.38h-4.16v19.23h-21.14L263.97,38.76z M275.55,63.85l1.29,3.03
+        l9.55-4.05l-1.29-3.03L275.55,63.85z"/>
+      <path className="st0" d="M305.14,38.76h19.31l7.67,24.1l1.3-0.29l-1.04-4.87c-0.8-3.91-1.85-8.41-1.85-12.48v-6.47h21.39v59.85H332.6
+        l-7.67-24.1l-1.29,0.27l1.04,4.87c0.8,3.91,1.85,8.43,1.85,12.5v6.46h-21.39V38.76z"/>
+      <polygon className="st0" points="395.23,57.75 385.34,57.75 385.34,98.61 363.95,98.61 363.95,57.75 354.14,57.75 354.14,38.76 
+        395.23,38.76      "/>
+      <rect x="397.46" y="38.76" className="st0" width="21.39" height="59.85"/>
+      <path className="st1" d="M318.86,150.49c-0.44-2.22-0.76-5.21-0.76-7.88v-11.94c0-1.14-0.44-2.03-1.33-2.03h-1.4v21.85h-5.97V99.68
+        h7.69c2.98,0,6.73,1.53,6.73,8.01v12.96c0,2.42-1.14,3.94-2.73,5.27c1.59,1.33,2.86,2.86,2.86,5.27v10.8
+        c0,3.24,0.44,6.03,0.83,8.51H318.86z M317.97,107.81c0-1.46-0.38-2.03-1.08-2.03h-1.52v17.09h1.52c0.7,0,1.08-0.57,1.08-2.03
+        V107.81z"/>
+      <path className="st1" d="M333.6,151.13c-4.13,0-7.18-3.56-7.18-8.7V99.68h5.66v43.19c0,1.14,0.57,1.84,1.52,1.84
+        c1.02,0,1.59-0.7,1.59-1.84V99.68h5.4v42.75C340.59,147.57,337.73,151.13,333.6,151.13"/>
+      <path className="st1" d="M264.94,151.13c-4.13,0-7.3-3.56-7.3-8.7v-34.68c0-5.15,3.18-8.7,7.3-8.7c4.13,0,7.31,3.56,7.31,8.7v12.08
+        l-5.78-1.66V107.3c0-1.14-0.57-1.84-1.53-1.84c-0.95,0-1.52,0.7-1.52,1.84v35.57c0,1.14,0.57,1.84,1.52,1.84
+        c0.95,0,1.53-0.7,1.53-1.84v-10.86h5.78v10.42C272.25,147.57,269.07,151.13,264.94,151.13"/>
+      <path className="st1" d="M282.16,151.13c-4.13,0-7.56-3.56-7.56-8.7v-34.68c0-5.15,3.43-8.7,7.56-8.7c4.13,0,7.56,3.56,7.56,8.7
+        v34.68C289.71,147.57,286.28,151.13,282.16,151.13 M283.68,107.3c0-1.14-0.57-1.84-1.52-1.84c-1.02,0-1.59,0.7-1.59,1.84v35.57
+        c0,1.14,0.57,1.84,1.59,1.84c0.95,0,1.52-0.7,1.52-1.84V107.3z"/>
+      <path className="st1" d="M301.72,150.49c-0.44-2.22-0.76-5.21-0.76-7.88v-11.94c0-1.14-0.44-2.03-1.33-2.03h-1.4v21.85h-5.97V99.68
+        h7.69c2.98,0,6.73,1.53,6.73,8.01v12.96c0,2.42-1.14,3.94-2.73,5.27c1.59,1.33,2.86,2.86,2.86,5.27v10.8
+        c0,3.24,0.44,6.03,0.83,8.51H301.72z M300.83,107.81c0-1.46-0.38-2.03-1.08-2.03h-1.52v17.09h1.52c0.7,0,1.08-0.57,1.08-2.03
+        V107.81z"/>
+      <path className="st1" d="M350.87,129.28h-1.71v21.22h-5.97V99.68h7.69c2.98,0,6.73,1.53,6.73,8.01v13.66
+        C357.61,127.75,353.86,129.28,350.87,129.28 M351.76,107.81c0-1.46-0.38-2.03-1.08-2.03h-1.52v17.4h1.52
+        c0.7,0,1.08-0.57,1.08-2.03V107.81z"/>
+      <path className="st1" d="M366.43,151.26c-4.13,0-7.3-3.56-7.3-8.7v-34.68c0-5.15,3.18-8.7,7.3-8.7c4.13,0,7.31,3.56,7.31,8.7v12.08
+        l-5.78-1.66v-10.86c0-1.14-0.57-1.84-1.53-1.84c-0.95,0-1.52,0.7-1.52,1.84V143c0,1.14,0.57,1.84,1.52,1.84
+        c0.95,0,1.53-0.7,1.53-1.84v-10.86h5.78v10.42C373.74,147.7,370.56,151.26,366.43,151.26"/>
+      <rect x="376.47" y="99.8" className="st1" width="5.97" height="50.82"/>
+      <path className="st1" d="M392.73,99.17c-4.13,0-7.56,3.56-7.56,8.7v34.68c0,5.14,3.43,8.7,7.56,8.7c4.13,0,7.56-3.56,7.56-8.7v-34.68
+        C400.29,102.73,396.86,99.17,392.73,99.17 M394.25,143c0,1.14-0.57,1.84-1.52,1.84c-1.02,0-1.59-0.7-1.59-1.84v-35.57
+        c0-1.14,0.57-1.84,1.59-1.84c0.95,0,1.52,0.7,1.52,1.84V143z"/>
+      <polygon className="st1" points="397.17,94.44 388.51,98.61 388.51,96.29 397.17,92.12      "/>
+      <polygon className="st1" points="413.88,150.62 407.78,122.86 407.78,150.62 402.82,150.62 402.82,99.8 408.86,99.8 413.94,128.01 
+        413.94,99.8 418.83,99.8 418.83,150.62       "/>
+      <polygon className="st0" points="418.83,150.62 413.88,150.62 257.64,105.91 257.64,99.04 418.82,145.16       "/>
+    </g>
+    <text transform="matrix(1 0 0 1 56.7549 65.0283)"><tspan x="0" y="0" className="st1 st19 st20">Secretaría</tspan><tspan x="19.01" y="24.83" className="st1 st19 st20">Ejecutiva</tspan></text>
+  </g>
+  <g>
+    <text transform="matrix(1 0 0 1 0 26.4189)" className="st1 st21 st22">Buscamos titular de la</text>
+  </g>
 </g>
+    <style jsx>{`
+  .st0{fill:#00BDFF;}
+  .st1{fill:#FFFFFF;}
+  .st2{opacity:0.74;clip-path:url(#SVGID_2_);}
+  .st3{fill:none;stroke:#3FA9F5;stroke-width:1.1803;stroke-miterlimit:10;}
+  .st4{fill:none;stroke:#3FA9F5;stroke-width:4.6846;stroke-miterlimit:10;}
+  .st5{fill:none;stroke:#3FA9F5;stroke-width:8.189;stroke-miterlimit:10;}
+  .st6{fill:none;stroke:#3FA9F5;stroke-width:11.6933;stroke-miterlimit:10;}
+  .st7{fill:none;stroke:#3FA9F5;stroke-width:15.1976;stroke-miterlimit:10;}
+  .st8{fill:none;stroke:#3FA9F5;stroke-width:18.702;stroke-miterlimit:10;}
+  .st9{fill:none;stroke:#3FA9F5;stroke-width:22.2063;stroke-miterlimit:10;}
+  .st10{fill:none;stroke:#3FA9F5;stroke-width:25.7107;stroke-miterlimit:10;}
+  .st11{fill:none;stroke:#3FA9F5;stroke-width:29.215;stroke-miterlimit:10;}
+  .st12{fill:none;stroke:#3FA9F5;stroke-width:32.7193;stroke-miterlimit:10;}
+  .st13{fill:none;stroke:#3FA9F5;stroke-width:36.2237;stroke-miterlimit:10;}
+  .st14{fill:none;stroke:#3FA9F5;stroke-width:39.728;stroke-miterlimit:10;}
+  .st15{fill:none;stroke:#3FA9F5;stroke-width:43.2324;stroke-miterlimit:10;}
+  .st16{opacity:0.74;clip-path:url(#SVGID_4_);}
+  .st17{opacity:0.74;clip-path:url(#SVGID_6_);}
+  .st18{opacity:0.74;clip-path:url(#SVGID_8_);}
+  .st19{font-family:'SpaceMono-Italic';}
+  .st20{font-size:29.8481px;}
+  .st21{font-family:'SpaceMono-BoldItalic';}
+  .st22{font-size:31.0817px;}
+  .st23{fill:#1A1B2F;}
+  .st24{opacity:0.74;clip-path:url(#SVGID_10_);}
+  svg {
+            margin-top: -5em;
+          }
+          @media only screen and (max-width: 990px) {
+            svg {
+              margin-top: -4em;
+            }
+          }
+          @media only screen and (max-width: 768px) {
+           
+          }
+  `}</style>
 </svg>
 
      
   );
 }
 
-export default Logo;
+export default ConvoSESEA;
