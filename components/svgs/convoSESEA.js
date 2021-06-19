ed0aac2edc060c713a910dc3e28d3e4f0383d478
cpcwebchih <73916365+cpcwebchih@users.noreply.github.com>
2021-06-19 03:07:41 +0000

    Update convoSESEA.js

--- components/svgs/convoSESEA.js
+++ components/svgs/convoSESEA.js
@@ -1,7 +1,7 @@
 const ConvoSESEA = ({size}) =>  {
   return (
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-   viewBox="0 0 418.85 151.26" style="enable-background:new 0 0 418.85 151.26;" xml:space="preserve">
+   viewBox="0 0 418.85 151.26"  width={size} height={size}>
 
 <g>
   <g>
