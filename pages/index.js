2b32ba5807af663c3b74328d21cd363721c84b3b
cpcwebchih <73916365+cpcwebchih@users.noreply.github.com>
2021-12-13 18:39:14 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import customtheme from '../customtheme';
 import Header from '../components/header';
 import { Text, Flex, Button} from '@chakra-ui/core';
 
+import ConvoCCPC from '../components/svgs/convoCCPC'
 import ConvoSESEA from '../components/svgs/convoSESEA'
 import YoElijoRegidor from '../components/svgs/yoelijoregidor'
 import InformeAnual from '../components/svgs/informeanual'
@@ -97,6 +98,29 @@ export default function Home() {
               <Flex direction="column" width="100%" justify="center" alignItems="center" py={["2em"]}>
                 <Flex direction="column" alignItems="center" transform="translateY(150px)" zIndex="50" mt="-10em">
                     <ConvoSESEA size={configs.regidorSize}/> 
+                </Flex>
+              </Flex>
+              <Flex fontFamily="cpc.gothamCondensed" lineHeight="1.1em" fontSize="1.2em" direction="column" width="100%" textAlign="center" alignItems="center" px="1em">
+                <Text fontFamily="cpc.gothamCondensed" mb={3}>
+                  En 2021 realizamos un proceso abierto de convocatoria para elegir la terna para titular de la Secretaría Ejecutiva.
+                </Text>
+                <Text fontFamily="cpc.gothamCondensed" mb={3}>
+                  Documentamos la totalidad del proceso conforme se fue llevando a cabo y te invitamos a que lo revises.
+                </Text>
+                <Link href="/publicaciones/convocatoriaSESEA2021">
+                <Button size="lg" bg={white} color="#00BDFF" rounded="10px" _hover={{ bg: "#00BDFF", color: "white" }} width={["250px", "200px", "200px", "200px"]}>
+                  <i aria-hidden="true" className="far fa-file-alt"></i>
+                    &nbsp; Revisa Aquí
+                </Button>
+                </Link>
+              </Flex>
+            </Flex>
+          </Section>
+          <Section bg="#1A1B2F" color={white} desktopWidth="100%">
+            <Flex direction={["column", "column", "row", "row"]} alignItems="center">
+              <Flex direction="column" width="100%" justify="center" alignItems="center" py={["2em"]}>
+                <Flex direction="column" alignItems="center" transform="translateY(150px)" zIndex="50" mt="-10em">
+                    <ConvoCCPC size={configs.regidorSize}/> 
                 </Flex>
               </Flex>
               <Flex fontFamily="cpc.gothamCondensed" lineHeight="1.1em" fontSize="1.2em" direction="column" width="100%" textAlign="center" alignItems="center" px="1em">
