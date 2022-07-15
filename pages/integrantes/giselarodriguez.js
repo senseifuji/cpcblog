fb431cb8169ecbc5f82b3c0374b9391b32837f10
cpcwebchih <73916365+cpcwebchih@users.noreply.github.com>
2022-07-15 14:56:19 -0600

    Add files via upload

---
+++ pages/integrantes/giselarodriguez.js
@@ -0,0 +1,44 @@
+import Layout from '../../components/layout'
+import Header from '../../components/header'
+import Content from '../../components/content';
+import Member from '../../components/member'
+import CpcSeo from '../../components/cpcseo';
+
+
+const GiselaRodriguezPage = () => {
+
+    const links = [
+       {title: "Contrato Anual", url: "#"},
+        {title: "Declaraciones 3 de 3 ", url: "#"},
+        {title: "Entrevista", url: "https://comiteanticorrupcion.mx/publicaciones/entrevistacarmen"},
+        {title: "Currículum", url: "https://comiteanticorrupcion.mx/publicaciones/cvcarmen"},
+        {title: "Nombramiento", url: "https://comiteanticorrupcion.mx/publicaciones/nombramientocarmen"},
+        {title: "Carta de Intención", url: "https://comiteanticorrupcion.mx/publicaciones/cartacarmen"}
+    ]
+
+    return ( 
+        <>
+            <CpcSeo 
+                title="Gisela Rodríguez - Comité Participativo Ciudadano de Chihuahua"
+                description="Conoce a Gisela Rodríguez, integrante del Comité Participativo Ciudadano de Chihuahua"
+                url={`${process.env.NEXT_PUBLIC_BASE_URL}/integrantes/giselarodriguez`}
+                imageUrl="https://github.com/cpcwebchih/cpcblog/blob/master/public/images/giselaportada.png?raw=true"
+            />
+            <Layout>
+                <Header position="fixed"/>
+                <Content>
+                    <Member 
+                        title="Gisela Rodríguez"
+                        role="Integrante"
+                        membersince="2022-2027"
+                        photouRL="/images/giselarodriguez.svg"
+                        mail="gisela@comiteanticorrupcion.mx"
+                        links={links}
+                    />
+                </Content>
+            </Layout>
+        </>
+     );
+}
+ 
+export default GiselaRodríguezPage;
