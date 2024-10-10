4cd6a9ce3f036cd62a3940b1a5100e4ac85d3e85
senseifuji <sensei@eskuelaradical.org>
2024-10-10 15:40:02 -0600

    Update IntegrantCard.js

--- components/IntegrantCard.js
+++ components/IntegrantCard.js
@@ -1,38 +1,42 @@
-import { Box, Text } from "@chakra-ui/react";
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import { Box, Image, Text, Link } from "@chakra-ui/react";
+import NextLink from 'next/link';
 
-const IntegrantCard = ({ name, role, imageUrl, profileUrl, isFormer = false }) => (
-  <Box
-    maxWidth="200px"
-    display="flex"
-    flexDirection="column"
-    backgroundColor="white"
-    p="16px"
-    m={2}
-    opacity={isFormer ? 0.6 : 1}
-    transition="opacity 0.3s"
-    _hover={{ opacity: 1 }}
-  >
-    <Link href={profileUrl || "#"} passHref legacyBehavior>
-      <a style={{ textDecoration: 'none' }}>
-        <Box maxWidth="200px" maxHeight="200px" pb="16px" textAlign="center">
-          <Image width={200} height={200} alt={name} src={imageUrl} />
-        </Box>
-        <Box maxWidth="200px">
-          <Text as="span" fontFamily='cpc.gothamCondensed' color="black" fontSize="27px" fontWeight="300" textAlign="left" isTruncated>
+const IntegrantCard = ({ name, role, imageUrl, profileUrl, isFormer = false }) => {
+  return (
+    <Box textAlign="center" mb={4} mx={4}> 
+      <NextLink href={profileUrl} passHref>
+        <Link>
+          <Image
+            src={imageUrl}
+            alt={name}
+            borderRadius="full"
+            boxSize="200px"
+            objectFit="cover"
+            mb={2}
+          />
+          <Text
+            fontWeight="bold"
+            fontSize="lg"
+            noOfLines={1}
+            width="200px"
+            mx="auto"
+          >
             {name}
           </Text>
-        </Box>
-        <Box maxWidth="200px">
-          <Text as="span" fontFamily='cpc.gothamCondensedBook' color="black" fontSize="27px" fontWeight="300" textAlign="left" isTruncated>
-            {role}
-          </Text>
-        </Box>
-      </a>
-    </Link>
-  </Box>
-);
+          {role && (
+            <Text fontSize="md" color="gray.600">
+              {role}
+            </Text>
+          )}
+          {isFormer && (
+            <Text fontSize="sm" color="gray.500" fontStyle="italic">
+              Expresidente
+            </Text>
+          )}
+        </Link>
+      </NextLink>
+    </Box>
+  );
+};
 
 export default IntegrantCard;
